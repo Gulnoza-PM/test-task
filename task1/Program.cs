4c030e85174075954ac37a45400987021a8197b2
Your_name <mgulnoza1805@mail.com>
2022-12-03 16:57:50 +0500

    f

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Hello! Please Enter your name");
+var name = Console.ReadLine();
+Console.Write("Hi, ");
+Console.Write(name);
+
+
+
+     
+   
