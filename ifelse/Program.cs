f9a67ae20e6c10cffdef361f5badc305585a0e28
Your_name <mgulnoza1805@mail.com>
2022-12-03 22:03:12 +0500

    ad if esle

---
+++ ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name");
+var name=Console.ReadLine();
+
+if(name.ToLower() =="gulnoza")
+{
+        Console.WriteLine("Uraaa Gulnoza welcome!");
+}
+else
+{
+   Console.Write("Oh, no ");
+   Console.Write(name); 
+}
