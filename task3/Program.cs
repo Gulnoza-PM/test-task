d3d12d0f6d0ed6f055c8a8846e08a957eb1e9ac6
Your_name <mgulnoza1805@mail.com>
2022-12-03 17:18:58 +0500

    random

---
+++ task3/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1 =new Random().Next(1,10);   // 1 2 3.....9
+Console.WriteLine(num1);
+
+int num2 =new Random().Next(1,10);
+
+Console.WriteLine(num2);
+ 
+ int result= num1+num2;
+ Console.WriteLine(result);
