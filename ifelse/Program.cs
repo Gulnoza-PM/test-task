7a9fc15148a91299c2aab43719e43cbe1570d929
Your_name <mgulnoza1805@mail.ru>
2022-12-17 20:18:36 +0500

    seminar 2 task

--- ifelse/Program.cs
+++ ifelse/Program.cs
@@ -1,12 +1,34 @@
-﻿Console.WriteLine("Enter your name");
-var name=Console.ReadLine();
+﻿//На вход принимает число А и видает сумму от 1 до А
 
-if(name.ToLower() =="gulnoza")
+Console.WriteLine("Enter number");
+int num=int.Parse(Console.ReadLine()!);
+
+
+int GetSumNum(int number)
+
 {
-        Console.WriteLine("Uraaa Gulnoza welcome!");
+        int sum=0;
+        while(number>0)
+        {
+                Console.WriteLine(number);
+                sum=sum+number;
+                number--;
+                
+        }
+        return sum;
 }
-else
-{
-   Console.Write("Oh, no ");
-   Console.Write(name); 
-}
+
+Console.WriteLine($"Sum of numbers: {GetSumNum (num)}");
+
+
+// var name=Console.ReadLine();
+
+// if(name.ToLower() =="gulnoza")
+// {
+//         Console.WriteLine("Uraaa Gulnoza welcome!");
+// }
+// else
+// {
+//    Console.Write("Oh, no ");
+//    Console.Write(name); 
+// }
