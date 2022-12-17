54437980293a705111ab4aa40c577ac80a1aed91
Your_name <mgulnoza1805@mail.ru>
2022-12-17 20:46:20 +0500

    Sem2/2

--- ifelse/Program.cs
+++ ifelse/Program.cs
@@ -1,34 +1,64 @@
-﻿//На вход принимает число А и видает сумму от 1 до А
+﻿// Get number and makes sum of the number
 
-Console.WriteLine("Enter number");
-int num=int.Parse(Console.ReadLine()!);
+Console.WriteLine("Enter number: ");
+int userInput=int.Parse(Console.ReadLine()!);
+
+int CountUserInputNumber(int number)
+ {
+int sum=0;
+while(number>0)
+ {
+       // Console.WriteLine(number);
+        number/=10;
+        sum++;
+
+ }
+ return sum;
+
+ }
+
+Console.WriteLine($"{CountUserInputNumber (userInput)}");
 
 
-int GetSumNum(int number)
 
-{
-        int sum=0;
-        while(number>0)
-        {
-                Console.WriteLine(number);
-                sum=sum+number;
-                number--;
+
+
+
+
+
+
+
+//На вход принимает число А и видает сумму от 1 до А
+
+// Console.WriteLine("Enter number");
+// int num=int.Parse(Console.ReadLine()!);
+
+
+// int GetSumNum(int number)
+
+// {
+//         int sum=0;
+//         while(number>0)
+//         {
+//                 Console.WriteLine(number);
+//                 sum=sum+number;
+//                 number--;
                 
-        }
-        return sum;
-}
-
-Console.WriteLine($"Sum of numbers: {GetSumNum (num)}");
-
-
-// var name=Console.ReadLine();
-
-// if(name.ToLower() =="gulnoza")
-// {
-//         Console.WriteLine("Uraaa Gulnoza welcome!");
+//         }
+//         return sum;
 // }
-// else
-// {
-//    Console.Write("Oh, no ");
-//    Console.Write(name); 
-// }
+
+// Console.WriteLine($"Sum of numbers: {GetSumNum (num)}");
+
+
+// // var name=Console.ReadLine();
+
+// // if(name.ToLower() =="gulnoza")
+// // {
+// //         Console.WriteLine("Uraaa Gulnoza welcome!");
+// // }
+// // else
+// // {
+// //    Console.Write("Oh, no ");
+// //    Console.Write(name); 
+// // }
